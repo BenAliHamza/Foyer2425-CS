7b193e74cd6c9976bec504efee428ed8ddb236c2
Hamza Ben Ali <hamza.benali@yellow-it.fr>
2025-01-03 11:25:41 +0100

    add new config

--- test.groovy
+++ test.groovy
@@ -202,7 +202,6 @@ pipeline {
                 }
             }
         }
-
         stage('Deploy Application') {
             steps {
                 script {
@@ -224,6 +223,7 @@ pipeline {
                 }
             }
         }
+
     }
 
     post {
